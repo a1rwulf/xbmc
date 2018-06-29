08d5390c4e43ce47ca5a660a094a9283b1ebaaad
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2018-06-29 09:43:40 +0200

    Windowing: GBM - close unused file descriptors

--- xbmc/windowing/gbm/DRMUtils.cpp
+++ xbmc/windowing/gbm/DRMUtils.cpp
@@ -450,6 +450,8 @@ bool CDRMUtils::OpenDrm(bool needConnector)
       {
         if(!GetResources())
         {
+          drmClose(m_fd);
+          m_fd = -1;
           continue;
         }
 
@@ -457,6 +459,8 @@ bool CDRMUtils::OpenDrm(bool needConnector)
         {
           if(!FindConnector())
           {
+            drmClose(m_fd);
+            m_fd = -1;
             continue;
           }
 
