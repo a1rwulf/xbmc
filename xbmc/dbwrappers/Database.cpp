df18c10d736f5db191f727967bb6f259b9ba3fa7
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2018-10-08 22:41:33 +0200

    Rename ProfilesManager -> ProfileManager (source files, class name, variable names)

--- xbmc/dbwrappers/Database.cpp
+++ xbmc/dbwrappers/Database.cpp
@@ -10,7 +10,7 @@
 #include "settings/AdvancedSettings.h"
 #include "filesystem/SpecialProtocol.h"
 #include "filesystem/File.h"
-#include "profiles/ProfilesManager.h"
+#include "profiles/ProfileManager.h"
 #include "settings/SettingsComponent.h"
 #include "utils/log.h"
 #include "utils/SortUtils.h"
@@ -140,7 +140,7 @@ bool CDatabase::ExistsSubQuery::BuildSQL(std::string & strSQL)
 }
 
 CDatabase::CDatabase() :
-  m_profileManager(*CServiceBroker::GetSettingsComponent()->GetProfilesManager())
+  m_profileManager(*CServiceBroker::GetSettingsComponent()->GetProfileManager())
 {
   m_openCount = 0;
   m_sqlite = true;
