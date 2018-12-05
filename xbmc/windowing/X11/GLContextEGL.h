oz23/kodi

X11: egl - make sure that we block in swapBuffers

xbmc/windowing/X11/GLContextEGL.h
@@ -23,6 +23,7 @@
 #if defined(HAVE_X11)
 #include "GLContext.h"
 #include "EGL/egl.h"
+#include "EGL/eglextchromium.h"
 #include <X11/Xutil.h>
 
 class CGLContextEGL : public CGLContext
@@ -44,6 +45,16 @@ public:
 protected:
   bool IsSuitableVisual(XVisualInfo *vInfo);
   EGLConfig getEGLConfig(EGLDisplay eglDisplay, XVisualInfo *vInfo);
+  PFNEGLGETSYNCVALUESCHROMIUMPROC eglGetSyncValuesCHROMIUM = nullptr;
+
+  struct Sync
+  {
+    uint64_t cont = 0;
+    uint64_t ust = 0;
+    uint64_t msc = 0;
+    uint64_t sbc = 0;
+    uint64_t interval = 0;
+  } m_sync;
 };
 
 #endif

a1rwulf/xbmc

xbmc/windowing/X11/GLContextEGL.h
@@ -22,6 +22,7 @@
 
 #include "GLContext.h"
 #include "EGL/egl.h"
+#include "EGL/eglextchromium.h"
 #include <X11/Xutil.h>
 
 class CGLContextEGL : public CGLContext
@@ -42,4 +43,16 @@ public:
 protected:
   bool IsSuitableVisual(XVisualInfo *vInfo);
   EGLConfig GetEGLConfig(EGLDisplay eglDisplay, XVisualInfo *vInfo);
+  PFNEGLGETSYNCVALUESCHROMIUMPROC eglGetSyncValuesCHROMIUM = nullptr;
+
+  struct Sync
+  {
+    uint64_t cont = 0;
+    uint64_t ust1 = 0;
+    uint64_t ust2 = 0;
+    uint64_t msc1 = 0;
+    uint64_t msc2 = 0;
+    uint64_t sbc2 = 0;
+    uint64_t interval = 0;
+  } m_sync;
 };