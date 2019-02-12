98e7c403249e084b1004951befdd8838999df14a
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2019-02-12 20:04:05 +0100

    Add new playlist odb object

---
+++ xbmc/interfaces/odb/ODBPlaylist.h
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2016-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#ifndef ODBPLAYLIST_H
+#define ODBPLAYLIST_H
+
+#include <odb/core.hxx>
+#include <odb/lazy-ptr.hxx>
+
+#include <string>
+#include <vector>
+
+class CODBSong;
+
+PRAGMA_DB (model version(1, 1, open))
+
+PRAGMA_DB (object pointer(std::shared_ptr) \
+                  table("playlist")
+                  session)
+class CODBPlaylist {
+public:
+PRAGMA_DB (id auto)
+    unsigned long m_idPlaylist;
+
+    std::string m_name;
+    std::string m_description;
+    std::string m_thumbUrl;
+
+    std::vector< std::shared_ptr<CODBSong> > m_songs;
+
+//Members not stored in the db, used for sync ...
+PRAGMA_DB (transient)
+    bool m_synced;
+
+private:
+    friend class odb::access;
+};
+
+#ifdef ODB_COMPILER
+#include "ODBSong.h"
+#endif
+
+#endif //ODBPLAYLIST_H
