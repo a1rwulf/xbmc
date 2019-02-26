7e9d3be01d167f18a2cf1ff8e18e4a82f1a50121
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2019-02-26 17:40:00 +0100

    [PVR][guiinfo] Fix/add support for ListItem.ParentalRating, VideoPlayer.Premiered, ListItem.Rating, VideoPlayer.Rating for PVR items.
    (cherry picked from commit 89bc0df51b8ed16b27f0248a8d1facab2cc2bfa7)

--- xbmc/pvr/PVRGUIInfo.cpp
+++ xbmc/pvr/PVRGUIInfo.cpp
@@ -566,13 +566,14 @@ bool CPVRGUIInfo::GetListItemAndPlayerLabel(const CFileItem *item, const CGUIInf
         strValue = epgTag->Icon();
         return true;
       case VIDEOPLAYER_PARENTAL_RATING:
-      case LISTITEM_PARENTALRATING:
+      case LISTITEM_PARENTAL_RATING:
         if (epgTag->ParentalRating() > 0)
         {
           strValue = StringUtils::Format("%i", epgTag->ParentalRating());
           return true;
         }
         return false;
+      case VIDEOPLAYER_PREMIERED:
       case LISTITEM_PREMIERED:
         if (epgTag->FirstAiredAsLocalTime().IsValid())
         {
@@ -580,6 +581,17 @@ bool CPVRGUIInfo::GetListItemAndPlayerLabel(const CFileItem *item, const CGUIInf
           return true;
         }
         return false;
+      case VIDEOPLAYER_RATING:
+      case LISTITEM_RATING:
+      {
+        int iStarRating = epgTag->StarRating();
+        if (iStarRating > 0)
+        {
+          strValue = StringUtils::FormatNumber(iStarRating);
+          return true;
+        }
+        return false;
+      }
     }
   }
 
